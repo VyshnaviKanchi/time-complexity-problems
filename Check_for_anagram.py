07927aabf3dd0b2265820d1d21e85878c0cfe490
VyshnaviKanchi <vyshnavi0kanchi@gmail.com>
2026-06-29 09:40:56 +0530

    Update Check_for_anagram.py

--- Check_for_anagram.py
+++ Check_for_anagram.py
@@ -1,10 +1,20 @@
 n1 = list(input())
 n2 = list(input())
+char = {}
 B = True
-for i in set(n1):
-    if n1.count(i) != n2.count(i):
+for ch in n1:
+    if ch not in char:
+        char[ch] = 0
+    char[ch] += 1
+for ch in n2:
+    if ch not in char or char[ch]  == 0:
         B = False
         break
+    char[ch] -= 1
+
+for ch in char.values():
+    if ch != 0:
+        B = False
 if len(n1) != len(n2):
     B = False
 if B:
